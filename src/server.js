b46811111ccd9c93e69f89e3fc2039d11e58f36f
Alen <alechkr@gmail.com>
2020-07-11 02:08:53 +0200

    adds data results to index.ejs

--- src/server.js
+++ src/server.js
@@ -31,8 +31,24 @@ app.listen(5000, () => {
 // Empty object defined to store the API data received with the post request
 // This get request gets executed whenever the page gets refreshed
 app.get('/', (req, res) => {  
-    let data = {name: "", country: "", ipo: "", finnhubindustry: "", weburl: ""};
-    res.render('index.ejs', {data});
+    /* let data = {name: "", country: "", ipo: "", finnhubindustry: "", weburl: ""};
+    res.render('index.ejs', {data}); */
+    let finalResult = [];
+    finalResult.push({
+        name: "", 
+        country: "", 
+        ipo: "", 
+        finnhubindustry: "", 
+        weburl: "", 
+        ticker: "", 
+        currency: ""
+    },
+    {
+        c: ""    
+    });
+
+    res.render('index.ejs', {finalResult});
+
 });
 
 // Get stock data from the Finnnhub API based on user stock ticker input
@@ -45,15 +61,10 @@ const api_key = finnhub.ApiClient.instance.authentications['api_key'];
 api_key.apiKey = process.env.API; 
 const finnhubClient = new finnhub.DefaultApi();
 
-// Stock candles
-/*   let timeframe = ['1', '5', '15', '30', '60', 'D', 'W', 'M'];
-finnhubClient.stockCandles(stock_ticker, timeframe[6], 1590988249, 1591852249, {}, (error, data, response) => {
-    res.send(data);
-}); */  
-
 // Store all data received from finnhub API in an array
 let finalResult = [];
 
+// General data about the company
 let companyProfile = () => {
     let promise = new Promise((res, rej) => {
         finnhubClient.companyProfile2({'symbol': stock_ticker}, (error, data, response) => {  
@@ -65,22 +76,98 @@ let companyProfile = () => {
     return promise;
 };
 
+// Actual stock price
 let stockQuote = () => {
     let promise = new Promise((res, rej) => {
-        finnhubClient.quote("AAPL", (error, data, response) => {
+        finnhubClient.quote(stock_ticker, (error, data, response) => {
             res(finalResult.push(data));
         }); 
     });
     return promise;
 };    
 
+// Stock ticker of similar companies
+let stockPeers = () => {
+    let promise = new Promise((res, rej) => {
+        finnhubClient.companyPeers(stock_ticker, (error, data, response) => {
+            res(finalResult.push(data));
+        }); 
+    });
+    return promise;
+};  
+
+// Stock price development at closing price over a timeframe
+let stockCandles = () => {
+    // Convert current date to Unix Timestamp
+    let currentDate = new Date();
+    let dateTimestamp = Math.trunc(currentDate/1000);
+    
+    console.log(currentDate);
+    console.log(dateTimestamp);
+    let promise = new Promise((res, rej) => {
+        let timeframe = ['1', '5', '15', '30', '60', 'D', 'W', 'M'];
+        finnhubClient.stockCandles(stock_ticker, timeframe[7],1577836800, dateTimestamp , {}, (error, data, response) => {
+            res(finalResult.push(data.t, data.c));
+        });
+        //1594339200
+    });
+    return promise;
+};  
+
+// Basic financial data
+let stockFinancials = () => {
+    let promise = new Promise((res, rej) => {
+        finnhubClient.companyBasicFinancials(stock_ticker, "margin", (error, data, response) => {
+            res(finalResult.push(data));
+        });
+    });
+    return promise;
+};  
+ 
+// Analyst stock recommendations
+let stockRecommendation = () => {
+    let promise = new Promise((res, rej) => {
+        finnhubClient.recommendationTrends(stock_ticker, (error, data, response) => {
+            res(finalResult.push(data[0]));
+        });
+    });
+    return promise;
+}; 
+
+// Stock recommendation based on technical analysis
+let stockTechnical = () => {
+    let promise = new Promise((res, rej) => {
+        finnhubClient.aggregateIndicator(stock_ticker, "D", (error, data, response) => {
+        res(finalResult.push(data));
+        });
+    });
+    return promise;
+}; 
+
+// Quarterly EPS
+let stockEarnings= () => {
+    let promise = new Promise((res, rej) => {
+        finnhubClient.companyEarnings(stock_ticker, {'limit': 10}, (error, data, response) => {
+        res(finalResult.push(data));
+        });
+    });
+    return promise;
+}; 
+
+// Data send to the client after all promises are resolved
 let sendToClient = () => {
-    res.send(finalResult);
+    res.render('index.ejs', {finalResult})
+    /* res.send(finalResult);  */
 };  
  
 companyProfile()
     .then(stockQuote)
-    .then(sendToClient);
-   
+    .then(stockPeers)
+    .then(stockCandles)
+    .then(stockFinancials)
+    .then(stockRecommendation)
+    .then(stockTechnical)
+    .then(stockEarnings)
+    .then(sendToClient); 
 }); 
 
